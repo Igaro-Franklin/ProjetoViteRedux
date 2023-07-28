3d1d7fdce3f3c0eeddf59afa66936da0f4187b8f
Igaro-Franklin <igarofranklinr@gmail.com>
2023-07-28 20:15:47 -0300

    Enviando a funcionalidade de adicionar e deletar endereços

--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -1,14 +1,68 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null
+  user: null
 }
 
 export const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reduces:{
+  name: "user",
+  initialState,
+  reducers: { 
+    
+    createUser: (state, action) => { 
 
+      return { 
+            ...state,
+            user:{
+                name: action.payload.name,
+                email: action.payload.email,
+                address:  null,
+            }
+        };
+
+    },
+    logoutUser: (state) =>{
+      return {
+        ...state,
+        user:null,
+      }
+    },
+    addAddress: (state, action) =>{
+      if(action.payload.location === "" || action.payload.number === ""){
+        alert("Preencha todoas os campos")
+        return{...state}
+      }
+
+      if(state.user === null){
+        alert("Faça o login para cadastrar um enderreço")
+
+        return {...state}
+      }
+
+      alert("Dados atualizados com sucesso!")
+      return{
+        ...state,
+        user: {
+          ...state.user,
+          address:{
+            location: action.payload.location,
+            number: action.payload.number,
+          }
+        }
+      }
+    },
+    deleteAddress: (state) =>{
+      return{
+        ...state,
+        user:{
+          ...state.user,
+          address: null,
+        }
+      }
     }
+  }
 })
+
+export const { createUser, logoutUser, addAddress, deleteAddress } = userSlice.actions;
+
 export default userSlice.reducer;
